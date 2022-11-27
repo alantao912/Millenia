8bbb8845d7ca60b7e1dd4d6f82bea82b919c8900
Alan Tao <alantao912@protonmail.ch>
2022-11-26 20:39:59 -0500

    Added test grammar for conditional and loops

--- muh_language/main.py
+++ muh_language/main.py
@@ -1,8 +1,14 @@
 from lark import Lark
 
 l = Lark('''start: WORD "," WORD "!"
-
             %import common.WORD   // imports from terminal library
             %ignore " "           // Disregard spaces in text
+            
+            
+            ?conditional_branch: "if" "(" BOOL_EXPR ")" STATEMENT 
+                               | "if" "(" BOOL_EXPR ")" BLOCK
+            ?while_loop: "while" "(" BOOL_EXPR ")" STATEMENT
+                       | "while" "(" BOOL_EXPR ")" BLOCK
+            ?for_loop: 
          ''')
 print(l.parse("Hello, World!"))
